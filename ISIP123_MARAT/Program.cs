905e37613ee400dcf046a71a4101739d80a2123b
svagatut123 <maratkhudayberdin7@gmail.com>
2025-09-24 15:27:23 +0300

    НАЧАЛО ПОЛОЖЕНО!!!

--- ISIP123_MARAT/Program.cs
+++ ISIP123_MARAT/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//практика 3
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+
+class Program
+{
+    class StatText
+    {
+        public string Text { get; set; }
+        public int WordCount { get; set; }
+        public string ShortWord { get; set; }
+        public string LongWord { get; set; }
+        public int SentencesCount { get; set; }
+        public int GlasCount { get; set; }
+        public int SoglasCount { get; set; }
+        public Dictionary <char, int> LetterFrequency { get; set; }
+        public DateTime AnalysisDate { get; set; }
+    }
+}
