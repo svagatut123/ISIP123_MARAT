d02e388618784a0c05c0f0f74cb69cce1c94ea71
svagatut123 <maratkhudayberdin7@gmail.com>
2025-09-24 15:53:53 +0300

    почти готов анализ текста

--- ISIP123_MARAT/Program.cs
+++ ISIP123_MARAT/Program.cs
@@ -18,7 +18,62 @@ class Program
         public int SentencesCount { get; set; }
         public int GlasCount { get; set; }
         public int SoglasCount { get; set; }
-        public Dictionary <char, int> LetterFrequency { get; set; }
-        public DateTime AnalysisDate { get; set; }
+        public Dictionary<char, int> LetterFrequency { get; set; } // char - буквы, int - их кол-во
+        public DateTime AnalysisDate { get; set; } // для просмотра анализа прошлых текстов
+
+        public StatText()
+        {
+            LetterFrequency = new Dictionary<char, int>();
+        }
     }
+    static List<StatText>
+        allStat = new List<StatText>();
+
+    static char[] Glas = { 'а', 'e', 'ё', 'и', 'о', 'ы', 'у', 'э', 'ю', 'я' };
+    static char[] SentencesCount = { '.', '?', '!', ';' };
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("--анализ текста--");
+        bool ContWork = true;
+        while (ContWork)
+        {
+            ShowMainMenu();
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    AnalyzeNewText;
+                    break;
+                case "2":
+                    ShowAllStat;
+                    break;
+                case "3":
+                    ContWork = false;
+                    Console.WriteLine("конец программы");
+                    break;
+                default:
+                    Console.WriteLine("ошибка");
+                    break;
+            }
+        }
+    }
+    static void ShowMainMenu()
+    {
+        Console.WriteLine("--Главное меню--");
+        Console.WriteLine("1. анализировать новый текст");
+        Console.WriteLine("2. показать статистику по прошлым текстам");
+        Console.WriteLine("3. выйти");
+        Console.Write("выбрать цифру: ");
+    }
+    static void AnalyzeNewText()
+    {
+        Console.WriteLine("--Анализ нового текста--");
+        string text;
+        while (true)
+        {
+            Console.WriteLine("введите текст (мин 100 символов");
+        }
+    }
+
 }
