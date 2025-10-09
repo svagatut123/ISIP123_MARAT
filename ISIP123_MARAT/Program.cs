7b4c695aeb56778719f56aec4a135d768e5b856a
svaga_tut123 <maratkhudayberdin7@gmail.com>
2025-10-09 11:54:53 +0300

    1 пара

--- ISIP123_MARAT/Program.cs
+++ ISIP123_MARAT/Program.cs
@@ -1,2 +1,36 @@
 ﻿//ПР5
 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public abstract class Person
+{
+    private int Personid;
+    private string Name;
+    private int Age;
+    private string Email;
+
+    private Person(int personid, string name, int age, string email)
+    {
+        Personid = personid;
+        Name = name;
+        Age = age;
+        Email = email;
+    }
+    public abstract string GetRole();
+
+    public virtual string GetInfo()
+    {
+        return $"id: {Personid}, имя: {Name}, возраст: {Age}, email: {Email}";  
+    }
+}
+public interface MyDisplay
+{
+    string DisplayInfo();
+}
+public class Student
+{
+
+}
