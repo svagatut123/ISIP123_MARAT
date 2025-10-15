2451ef7f622ed7b064e0814ac62021b5190a6df1
svagatut123 <maratkhudayberdin7@gmail.com>
2025-10-15 14:53:36 +0300

    class Item, class Wepon

--- ISIP123_MARAT/Program.cs
+++ ISIP123_MARAT/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// пр6
+using System;
+using System.Numerics;
+
+namespace Game
+{
+
+    public enum EnemyType
+    {
+        Goblin,
+        Skeleton,
+        Mage,
+        Boss
+    }
+    //типы врагов
+    public abstract class Item
+    {
+        public string Name { get; protected set; }
+        public int Value { get; protected set; }
+
+        protected Item(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public abstract void ApplyEffect(Player player);
+    }
+
+    public class Weapon : Item
+    {
+        public int Attack { get; private set; }
+
+        public Weapon(string name, int value, int attack) : base(name, value)
+        {
+            Attack = attack;
+        }
+
+        public override void ApplyEffect(Player player)
+        {
+            player.EquipWeapon(this);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (атака: {Attack}, ценность: {Value})";
+        }
+    }
+}
