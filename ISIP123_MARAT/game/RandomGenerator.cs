6d3f2a46267f26a3f46335da4af5571021460f95
svagatut123 <maratkhudayberdin7@gmail.com>
2025-11-20 14:45:08 +0300

    работает!!!!

--- ISIP123_MARAT/game/RandomGenerator.cs
+++ ISIP123_MARAT/game/RandomGenerator.cs
@@ -5,67 +5,46 @@ using System.Text;
 using System.Threading.Tasks;
 using ISIP123_MARAT.game;
 
-        public static class RandomGenerator
-        {
-            private static Random _random = new Random();
-            private static object _lock = new object();
+public static class RandomGenerator
+{
+    private static Random _random = new Random();
 
-            public static int Next()
-            {
-                lock (_lock)
-                {
-                    return _random.Next();
-                }
-            }
+    public static int Next()
+    {
+        return _random.Next();
+    }
 
-            public static int Next(int maxValue)
-            {
-                lock (_lock)
-                {
-                    return _random.Next(maxValue);
-                }
-            }
+    public static int Next(int maxValue)
+    {
+        return _random.Next(maxValue);
+    }
 
-            public static int Next(int minValue, int maxValue)
-            {
-                lock (_lock)
-                {
-                    return _random.Next(minValue, maxValue);
-                }
-            }
+    public static int Next(int minValue, int maxValue)
+    {
+        return _random.Next(minValue, maxValue);
+    }
 
-            public static double NextDouble()
-            {
-                lock (_lock)
-                {
-                    return _random.NextDouble();
-                }
-            }
+    public static double NextDouble()
+    {
+        return _random.NextDouble();
+    }
 
-            public static bool NextBool()
-            {
-                lock (_lock)
-                {
-                    return _random.Next(2) == 0;
-                }
-            }
+    public static bool NextBool()
+    {
+        return _random.Next(2) == 0;
+    }
 
-            public static T GetRandomItem<T>(T[] array)
-            {
-                lock (_lock)
-                {
-                    return array[_random.Next(array.Length)];
-                }
-            }
+    public static T GetRandomItem<T>(T[] array)
+    {
+        return array[_random.Next(array.Length)];
+    }
 
-            public static T GetRandomItem<T>(List<T> list)
-            {
-                lock (_lock)
-                {
-                    return list[_random.Next(list.Count)];
-                }
-            }
-        }
+    public static T GetRandomItem<T>(List<T> list)
+    {
+        return list[_random.Next(list.Count)];
+    }
+}
+
     
 
 
